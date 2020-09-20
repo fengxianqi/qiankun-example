f1fc4b50895650078679fd5ae0315249a85c99dd
fengxianqi <609587734@qq.com>
2020-09-20 23:23:56 +0800

    feat: main add listenRouterChange

--- main/src/App.vue
+++ main/src/App.vue
@@ -37,18 +37,43 @@ export default {
   methods: {
     goto (item) {
       history.pushState(null, item.activeRule, item.activeRule)
-      this.current = item.name
+      // this.current = item.name
     },
     bindCurrent () {
+      console.log('===============')
       const path = window.location.pathname
       if (this.microApps.findIndex(item => item.activeRule === path) >= 0) {
         this.current = path
       }
+    },
+    listenRouterChange () {
+      const _wr = function (type) {
+        const orig = history[type]
+        return function () {
+          const rv = orig.apply(this, arguments)
+          const e = new Event(type)
+          e.arguments = arguments
+          window.dispatchEvent(e)
+          return rv
+        }
+      }
+      history.pushState = _wr('pushState')
+
+      window.addEventListener('pushState', this.bindCurrent)
+      window.addEventListener('popstate', this.bindCurrent)
+
+      this.$once('hook:beforeDestroy', () => {
+        window.removeEventListener('pushState', this.bindCurrent)
+        window.removeEventListener('popstate', this.bindCurrent)
+      })
     }
   },
   created () {
     this.bindCurrent()
     NProgress.start()
+  },
+  mounted () {
+    this.listenRouterChange()
   }
 }
 </script>
