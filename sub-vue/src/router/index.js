1cf33bfad457ba2601082018b27225cb8372416b
fengxianqi <fengxianqi12@gmail.com>
2022-05-16 11:35:10 +0800

    chore: 异步路由转同步

--- sub-vue/src/router/index.js
+++ sub-vue/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
 
 Vue.use(VueRouter)
 
@@ -16,7 +17,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
   }
 ]
 
